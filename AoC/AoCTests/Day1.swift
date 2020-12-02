a72cdebaabbb33ab5480818eba874ceb8e76098d
Mike Abdullah <me@mikeabdullah.net>
2020-12-02 08:04:57 +0000

    Part 1

--- AoC/AoCTests/Day1.swift
+++ AoC/AoCTests/Day1.swift
@@ -11,13 +11,10 @@ import XCTest
 class AoCTests: XCTestCase {
     
     func testPart1() {
-        
-    }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         measure {
-            // Put the code you want to measure the time of here.
+            let entries = Set(input)
+            let (a, b) = entries.pair(summingTo: 2020)!
+            print(a*b)
         }
     }
     
