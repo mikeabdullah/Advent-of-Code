0ca5883ce3cdeba7c83efee648257e67ce35ed9e
Mike Abdullah <me@mikeabdullah.net>
2020-12-02 08:44:28 +0000

    Parse input and solve part 1

--- AoC/AoCTests/Day 2/Day2.swift
+++ AoC/AoCTests/Day 2/Day2.swift
@@ -10,7 +10,48 @@ import XCTest
 
 class Day2: XCTestCase {
 
-    func testPart1 {
+    func testPart1() throws {
+        let location = Bundle(for: Day2.self).url(forResource: "input-2", withExtension: "txt")!
+        let input = try String(contentsOf: location)
+        let lines = input.split(separator: "\n")
         
+        let passwordsByPolicy: [Policy: Substring] = lines.reduce(into: [:], { dict, line in
+            let separation = line.range(of: ": ")!
+            let password = line.suffix(from: separation.upperBound)
+            let policy = Policy(line.prefix(upTo: separation.lowerBound))
+            dict[policy] = password
+        })
+        
+        measure {
+            
+            let validPasswords = passwordsByPolicy.lazy.filter { policy, password in
+                policy.validate(password)
+            }
+
+            print(validPasswords.count)
+        }
+    }
+    
+    struct Policy : Hashable {
+        /// The letter the policy controls
+        let character: Character
+        /// The max and min times the letter can appear
+        let range: ClosedRange<Int>
+        
+        init(_ string: Substring) {
+            let space = string.range(of: " ")!
+            self.character = string[space.upperBound]
+            
+            let rangeString = string.prefix(upTo: space.lowerBound)
+            let separator = rangeString.range(of: "-")!
+            let lowerBound = Int(rangeString.prefix(upTo: separator.lowerBound))!
+            let upperBound = Int(rangeString.suffix(from: separator.upperBound))!
+            self.range = lowerBound...upperBound
+        }
+        
+        func validate(_ password: Substring) -> Bool {
+            let occurrences = password.lazy.filter { $0 == self.character } .count
+            return range.contains(occurrences)
+        }
     }
 }
