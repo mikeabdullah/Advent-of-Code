a2ae75cdbe78e5fec8135dea705affd93b59679b
Mike Abdullah <me@mikeabdullah.net>
2020-12-02 08:54:43 +0000

    Factor out helpers to do counting more directly

--- AoC/AoCTests/Day 2/Day2.swift
+++ AoC/AoCTests/Day 2/Day2.swift
@@ -50,8 +50,29 @@ class Day2: XCTestCase {
         }
         
         func validate(_ password: Substring) -> Bool {
-            let occurrences = password.lazy.filter { $0 == self.character } .count
+            let occurrences = password.count(of: character)
             return range.contains(occurrences)
         }
+    }
+}
+
+
+extension Sequence {
+    
+    /// Counts how many elements match `predicate`.
+    func count(where predicate: (Element) -> Bool) -> Int {
+        var result = 0
+        for element in self where predicate(element) {
+            result += 1
+        }
+        return result
+    }
+}
+
+extension Sequence where Element : Comparable {
+    
+    /// Counts how many times an element appears.
+    func count(of element: Element) -> Int {
+        return count(where: { $0 == element })
     }
 }
