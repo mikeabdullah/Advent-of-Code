ffd890068b3b1471cf20772cb11fe99a343c2b6e
Mike Abdullah <me@mikeabdullah.net>
2020-12-02 08:16:39 +0000

    New group for day 2

---
+++ AoC/AoCTests/Day 2/Day2.swift
@@ -0,0 +1,16 @@
+//
+//  Day2.swift
+//  AoCTests
+//
+//  Created by Mike on 02/12/2020.
+//  Copyright © 2020 Mike Abdullah. All rights reserved.
+//
+
+import XCTest
+
+class Day2: XCTestCase {
+
+    func testPart1 {
+        
+    }
+}
