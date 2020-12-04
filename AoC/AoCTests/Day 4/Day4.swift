93a37c5a8409e3658eb0d07213e042594f5a570e
Mike Abdullah <me@mikeabdullah.net>
2020-12-04 07:12:26 +0000

    Solve part 1 crudely

--- AoC/AoCTests/Day 4/Day4.swift
+++ AoC/AoCTests/Day 4/Day4.swift
@@ -10,26 +10,48 @@ import XCTest
 
 class Day4: XCTestCase {
     
-    var batches: [Substring]!
+    var data: [PassportData]!
 
     override func setUpWithError() throws {
         let location = Bundle(for: Day2.self).url(forResource: "input-4", withExtension: "txt")!
         var input = try String(contentsOf: location)
         input = input.replacingOccurrences(of: "\n\n", with: "§")
         input = input.replacingOccurrences(of: "\n", with: " ")
-        batches = input.split(separator: "§")
+        let batches = input.split(separator: "§")
+        
+        self.data = batches.map { PassportData($0) }
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPart1() throws {
+        let numberValid = data.count(where: { $0.validate() })
+        XCTAssertEqual(numberValid, 219)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    struct PassportData {
+        init(_ string: Substring) {
+            
+            let regex = try! NSRegularExpression(pattern: #"(.+):(.+)"#, options: [])
+            
+            // Tear into fields
+            var fields: [Substring: Substring] = [:]
+            for field in string.split(separator: " ") {
+                let string = String(field)
+                let match = regex.firstMatch(in: string, options: [], range: NSMakeRange(0, string.utf16.count))!
+                let key = string[Range(match.range(at: 1), in: string)!]
+                let value = string[Range(match.range(at: 2), in: string)!]
+                fields[key] = value
+            }
+            
+            self.fields = fields
+        }
+        
+        let fields: [Substring: Substring]
+        
+        func validate() -> Bool {
+            var keys = Set(fields.keys)
+            keys.remove("cid")
+            
+            return keys == ["byr", "iyr", "eyr", "iyr", "hgt", "hcl", "ecl", "pid"]
         }
     }
-
 }
