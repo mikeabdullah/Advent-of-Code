76e3343f205e8bc1fa3804985bd0abc7afe4d2ab
Mike Abdullah <me@mikeabdullah.net>
2020-12-05 09:47:09 +0000

    Read in codes

--- AoC/AoCTests/Day 5/Day5.swift
+++ AoC/AoCTests/Day 5/Day5.swift
@@ -50,11 +50,51 @@ struct SeatCoordinate : Equatable {
     
     init?(boardingPassCode: String) {
         
+        var rowRange = 0..<128
+        for char in boardingPassCode.prefix(7) {
+            switch char {
+            case "F": rowRange = rowRange.binarySpacePartition(.lower)
+            case "B": rowRange = rowRange.binarySpacePartition(.higher)
+            default: return nil
+            }
+        }
+        assert(rowRange.lowerBound == rowRange.upperBound - 1)
         
-        return nil
+        var columnRange = 0..<8
+        for char in boardingPassCode.suffix(3) {
+            switch char {
+            case "L": columnRange = columnRange.binarySpacePartition(.lower)
+            case "R": columnRange = columnRange.binarySpacePartition(.higher)
+            default: return nil
+            }
+        }
+        assert(columnRange.lowerBound == columnRange.upperBound - 1)
+        
+        self = SeatCoordinate(row: rowRange.lowerBound, column: columnRange.lowerBound)
     }
     
     var seatID: Int {
         row * 8 + column
     }
+}
+
+
+extension Range where Bound == Int {
+    
+    var mid: Bound {
+        lowerBound + (upperBound - lowerBound) / 2
+    }
+    
+    /// Returns the lower of higher half of the range, according to `half`.
+    func binarySpacePartition(_ half: BinarySpacePartitionHalf) -> Self {
+        switch half {
+        case .lower: return self.lowerBound..<self.mid
+        case .higher: return self.mid..<self.upperBound
+        }
+    }
+}
+
+enum BinarySpacePartitionHalf {
+    case lower
+    case higher
 }
