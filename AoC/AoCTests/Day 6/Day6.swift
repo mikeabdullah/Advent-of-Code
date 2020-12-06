9b0c9f0517a8bf9c093572642602435d334e4ba8
Mike Abdullah <me@mikeabdullah.net>
2020-12-06 09:00:24 +0000

    Routine to split around substrings

--- AoC/AoCTests/Day 6/Day6.swift
+++ AoC/AoCTests/Day 6/Day6.swift
@@ -18,5 +18,29 @@ class Day6: XCTestCase {
     }
 
     
+    func testPart1() {
+        let groups = input.split(separator: "\n\n")
+        
+        print(groups)
+    }
+}
 
+
+extension String {
+    
+    /// Split around a series of elements.
+    func split<S>(separator: S) -> [SubSequence] where S : StringProtocol {
+        
+        var chunks = [SubSequence]()
+        var chunk = self[startIndex..<endIndex]
+        
+        while let range = chunk.range(of: separator) {
+            chunks.append(chunk.prefix(upTo: range.lowerBound))
+            chunk = self.suffix(from: range.upperBound)
+        }
+
+        chunks.append(chunk)
+        
+        return chunks
+    }
 }
