034da6f7f6f6c5bd7e3c33d6086227abc892dd65
Mike Abdullah <me@mikeabdullah.net>
2020-12-27 13:40:19 +0000

    Attempt to state problem as array of links

--- AoC/AoCTests/Day 23/Day23.swift
+++ AoC/AoCTests/Day 23/Day23.swift
@@ -21,13 +21,12 @@ class Day23: XCTestCase {
     func testSample1() throws {
         
         // Make the cup circuit
-        var game = GameState("389125467", count: 9)
+        var game = GameState(seed: "389125467", count: 9)
         game.doMoves(100)
         
         // Turn into an answer
-        let resultCups = game.cups[1]!.sequenceClockwise().dropFirst().prefix(8)
-        let values = resultCups.lazy.map { $0.value }
-        let strings = values.lazy.map { String($0) }
+        let resultCups = game.cups(from: 1).dropFirst().prefix(8)
+        let strings = resultCups.lazy.map { String($0.rawValue) }
         let result: String = strings.joined()
         XCTAssertEqual(result, "67384529")
     }
@@ -35,13 +34,12 @@ class Day23: XCTestCase {
     func testPart1() throws {
         
         // Make the cup circuit
-        var game = GameState("562893147", count: 9)
+        var game = GameState(seed: "562893147", count: 9)
         game.doMoves(100)
                 
         // Turn into an answer
-        let resultCups = game.cups[1]!.sequenceClockwise().dropFirst().prefix(8)
-        let values = resultCups.lazy.map { $0.value }
-        let strings = values.lazy.map { String($0) }
+        let resultCups = game.cups(from: 1).dropFirst().prefix(8)
+        let strings = resultCups.lazy.map { String($0.rawValue) }
         let result: String = strings.joined()
         XCTAssertEqual(result, "38925764")
     }
@@ -49,118 +47,134 @@ class Day23: XCTestCase {
     func testPart2() {
         
         // Make the cup circuit
-        var game = GameState("562893147", count: 1000000)
+        var game = GameState(seed: "562893147", count: 1000000)
         game.doMoves(10000000)
         
-        let resultCups = game.cups[1]!.sequenceClockwise().dropFirst().prefix(2)
-        let values = resultCups.map { $0.value }
-        let result = values[0] * values[1]
+        let result1 = game.cup(after: 1)
+        let result2 = game.cup(after: result1)
+        let result = result1.rawValue * result2.rawValue
         XCTAssertEqual(result, 131152940564)
     }
     
     private struct GameState {
         
-        init(_ string: String, count: Int) {
+        init(seed: String, count: Int) {
             
-            var cups = [Int:Cup]()
-            cups.reserveCapacity(count)
+            var iterator = seed.makeIterator()
+            let first = Cup(character: iterator.next()!)!
             
-            self.current = Cup(value: Int(String(string.first!))!)
-            cups[current.value] = current
-            
-            // Make the other cups, connecting the chain as we go along
-            var previous = current
-            for valueChar in string.dropFirst() {
-                let value = Int(String(valueChar))!
-                let cup = Cup(value: value)
-                cups[value] = cup
-                previous.next = cup
-                previous = cup
+            self.links = [Int](unsafeUninitializedCapacity: count) { (buffer, initCount) in
+                
+                // Make the other cups, connecting the chain as we go along
+                var previous = first
+                while let valueChar = iterator.next() {
+                    let next = Cup(character: valueChar)!
+                    buffer[previous.index] = next.index
+                    initCount += 1
+                    previous = next
+                }
+                
+                // Keep going until we've reached count
+                for i in initCount + 1 ..< count {
+                    buffer[previous.index] = i
+                    initCount += 1
+                    previous = Cup(index: i)
+                }
+                
+                // Make the final connection back to start
+                buffer[previous.index] = first.index
+                initCount += 1
             }
             
-            // Keep going until we've reached count
-            for i in cups.count+1 ..< count+1 {
-                let cup = Cup(value: i)
-                cups[i] = cup
-                previous.next = cup
-                previous = cup
-            }
-            
-            // Make the final connection back to start
-            previous.next = current
-            
-            self.cups = cups
+            self.current = first
         }
         
-        /// All cups in the game.
-        let cups: [Int:Cup]
+        // MARK: Links
+        
+        /// For a cup whose `index = value - 1`, tells the index of the next cup clockwise from it.
+        var links: [Int]
+        
+        func indexOfCup(after cup: Cup) -> Int {
+            return links[cup.index]
+        }
+        
+        func cup(after cup: Cup) -> Cup {
+            return Cup(index: indexOfCup(after: cup))
+        }
+        
+        /// Sequence that walks the cups, with `cup` as the first in the sequence.
+        func cups(from cup: Cup) -> UnfoldFirstSequence<Cup> {
+            return sequence(first: cup) { (cup) -> Cup? in
+                return self.cup(after: cup)
+            }
+        }
+        
+        mutating func link(from cup: Cup, to next: Cup) {
+            links[cup.index] = next.index
+        }
+        
+        // MARK: Cups
         
         /// The "current" cup.
         var current: Cup
         
-        func doMove() {
+        mutating func doMove() {
             
             // Pick out the cups after the current cup
-            let held = current.removeNext(3)
-            let heldValues = held.sequenceClockwise().lazy.map { $0.value }
             
-            // Find the destination
-            var destinationValue = current.value
+            let firstHeld = cup(after: current)
+            let sequenceThroughNext = cups(from: firstHeld).prefix(4)
+            link(from: current, to: sequenceThroughNext.last!)
+            
+            let sequenceToNext = sequenceThroughNext.dropLast()
+            let lastHeld = sequenceToNext.last!
+            
+            // Figure out which cup will be the destination
+            var destination = current
             repeat {
-                destinationValue -= 1
-                if destinationValue < 1 { destinationValue = cups.count }
-            } while heldValues.contains(destinationValue)
-            let destination = cups[destinationValue]!
+                var destinationValue = destination.rawValue - 1
+                if destinationValue < 1 { destinationValue = links.count }
+                destination = Cup(rawValue: destinationValue)
+            } while sequenceToNext.contains(destination)
             
-            // Insert the removed cups
-            destination.insert(held)
+            // Insert the removed cups after the destination
+            link(from: lastHeld, to: cup(after: destination))
+            link(from: destination, to: firstHeld)
         }
         
         mutating func doMoves(_ count: Int) {
             for _ in 1...count {
                 doMove()
                 // Move onto next cup
-                current = current.next
+                self.current = cup(after: current)
             }
         }
     }
 
-    class Cup {
+    struct Cup : RawRepresentable, Equatable, ExpressibleByIntegerLiteral {
         
-        init(value: Int, next: Cup? = nil) {
-            self.value = value
-            self.next = next
+        init(index: Int) {
+            self.rawValue = index + 1
         }
         
-        let value: Int
-        
-        /// The next cup in the clockwise direction
-        var next: Cup!
-        
-        /// Sequence that walks the cups until one is nil (so typically forever)
-        func sequenceClockwise() -> UnfoldFirstSequence<Cup> {
-            return sequence(first: self) { $0.next }
+        init(rawValue: Int) {
+            precondition(rawValue >= 1)
+            self.rawValue = rawValue
         }
         
-        func insert(_ cup: Cup) {
-            let next = self.next
-            self.next = cup
-            cup.sequenceClockwise().last!.next = next
+        init(integerLiteral value: Int) {
+            self.init(rawValue: value)
         }
         
-        /// Removes the _k_ next cups in the chain, changing `next` to point to the cup after this amount.
-        func removeNext(_ k: Int) -> Cup {
-            
-            let next = self.next!
-            var iterator = self.sequenceClockwise().dropFirst(k).makeIterator()
-            
-            let lastToRemove = iterator.next()!
-            let replacement = iterator.next()!
-            
-            lastToRemove.next = nil
-            self.next = replacement
-            return next
+        init?(character: Character) {
+            guard let value = Int(String(character)) else { return nil }
+            self.rawValue = value
         }
+        
+        let rawValue: Int
+        
+        /// The index of the cup in the linked list. i.e. value - 1
+        var index: Int { rawValue - 1 }
     }
 }
 
