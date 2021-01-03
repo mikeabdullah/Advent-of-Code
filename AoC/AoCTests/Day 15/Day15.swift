a1bd7b37d0806f22bf7b2fc097efd7c5a762f2e3
Mike Abdullah <me@mikeabdullah.net>
2021-01-03 16:40:24 +0000

    Store game state using a dictionary instead
    Allows O(1) lookup if when number was last acessed, and that's what we care about

--- AoC/AoCTests/Day 15/Day15.swift
+++ AoC/AoCTests/Day 15/Day15.swift
@@ -19,7 +19,7 @@ class Day15: XCTestCase {
     }
 
     func testSample036() {
-        var game = GameState(startingNumbers: [0, 3, 6])
+        let game = Game(startingNumbers: [0, 3, 6])
         
         XCTAssertEqual(game.speakNext(), 0)
         XCTAssertEqual(game.speakNext(), 3)
@@ -33,88 +33,105 @@ class Day15: XCTestCase {
     }
     
     func testSample132() {
-        var game = GameState(startingNumbers: [1, 3, 2])
+        let game = Game(startingNumbers: [1, 3, 2])
         XCTAssertEqual(game.speak(turn: 2020), 1)
     }
     
     func testSample213() {
-        var game = GameState(startingNumbers: [2, 1, 3])
+        let game = Game(startingNumbers: [2, 1, 3])
         XCTAssertEqual(game.speak(turn: 2020), 10)
     }
     
     func testSample123() {
-        var game = GameState(startingNumbers: [1, 2, 3])
+        let game = Game(startingNumbers: [1, 2, 3])
         XCTAssertEqual(game.speak(turn: 2020), 27)
     }
     
     func testSample231() {
-        var game = GameState(startingNumbers: [2, 3, 1])
+        let game = Game(startingNumbers: [2, 3, 1])
         XCTAssertEqual(game.speak(turn: 2020), 78)
     }
     
     func testSample321() {
-        var game = GameState(startingNumbers: [3, 2, 1])
+        let game = Game(startingNumbers: [3, 2, 1])
         XCTAssertEqual(game.speak(turn: 2020), 438)
     }
     
     func testSample312() {
-        var game = GameState(startingNumbers: [3, 1, 2])
+        let game = Game(startingNumbers: [3, 1, 2])
         XCTAssertEqual(game.speak(turn: 2020), 1836)
     }
     
     // MARK: Real
     
     func testPart1() {
-        var game = GameState(startingNumbers: [2,15,0,9,1,20])
+        let game = Game(startingNumbers: [2,15,0,9,1,20])
         XCTAssertEqual(game.speak(turn: 2020), 1280)
     }
     
     func testPart2() {
-        var game = GameState(startingNumbers: [2,15,0,9,1,20])
+        let game = Game(startingNumbers: [2,15,0,9,1,20])
         game.reserveCapacity(30000000)
-        XCTAssertEqual(game.speak(turn: 30000000), 1280)
+        XCTAssertEqual(game.speak(turn: 30000000), 651639)
     }
     
-    struct GameState {
+    class Game {
         
         init(startingNumbers: [Int]) {
             precondition(!startingNumbers.isEmpty)
-            self.spoken = startingNumbers
+            startingNumbers.forEach(appendTurn)
         }
         
-        /// The numbers which have been spoken so far in the game.
-        private(set) var spoken: [Int]
+        // MARK: Turns
         
-        mutating func reserveCapacity(_ minimumCapacity: Int) {
-            spoken.reserveCapacity(minimumCapacity)
+        /// The turn we're currently on, and its number
+        private var currentTurn: Int = 0
+        private var currentNumber: Int?
+        
+        private var numbersByLastTurn: [Int : Int] = [:]
+        
+        /// Stores the next turn.
+        private func appendTurn(_ number: Int) {
+            if let current = currentNumber {
+                numbersByLastTurn[current] = currentTurn
+            }
+            self.currentTurn += 1
+            self.currentNumber = number
         }
+        
+        func reserveCapacity(_ minimumCapacity: Int) {
+            numbersByLastTurn.reserveCapacity(minimumCapacity)
+        }
+        
+        // MARK: Running the Game
         
         /// Figures out the next number to speak and appends it to the spoken list.
         @discardableResult
-        mutating func speakNext() -> Int {
+        func speakNext() -> Int {
             
-            let last = spoken.last!
-            
-            // Was the number spoken before that?
-            let allButLast = spoken.dropLast()
-            guard let previousIndex = allButLast.lastIndex(of: last) else {
-                spoken.append(0)
+            let next = self.age(of: currentNumber!)
+            appendTurn(next)
+            return next
+        }
+        
+        /// How many turns ago was `number` last spoken? 0 if not yet spoken.
+        private func age(of number: Int) -> Int {
+            guard let previous = numbersByLastTurn[number] else {
                 return 0
             }
-            
-            let age = spoken.distance(from: previousIndex, to: allButLast.endIndex)
-            spoken.append(age)
+            let age = self.currentTurn - previous
+            assert(age > 0)
             return age
         }
         
         /// Advances the game to at least the specified turn and returns the number for that turn. Tuns are 1-based.
-        mutating func speak(turn: Int) -> Int {
+        func speak(turn: Int) -> Int {
             
-            while spoken.count < turn {
+            while currentTurn < turn {
                 speakNext()
             }
             
-            return spoken[turn - 1]
+            return currentNumber!
         }
     }
 }
