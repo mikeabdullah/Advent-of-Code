c3fde3e2f588dfd0e0ba24c2cdfc538731e59ebf
Mike Abdullah <me@mikeabdullah.net>
2021-12-29 12:14:32 +0000

    Solve Day 1, part 1

--- AoC2021/AoC2021Tests/Day 1/Day1.swift
+++ AoC2021/AoC2021Tests/Day 1/Day1.swift
@@ -18,7 +18,75 @@ class Day1: XCTestCase {
   }
   
   func testPart1() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    let input = try PuzzleInput(named: "input1")
+   
+    var increases = 0
+    var window = input.integers.slidingWindow(length: 2)
+    
+    repeat {
+      if window[0] < window[1] {
+        increases += 1
+      }
+    } while window.advance()
+    
+    XCTAssertEqual(increases, 1564)
+  }
+}
+
+
+struct PuzzleInput {
+  
+  let lines: [Substring]
+  
+  init(named name: String) throws {
+    guard let url = Bundle(for: Day1.self).url(forResource: name, withExtension: "txt")
+    else { throw CocoaError(.fileNoSuchFile) }
+    
+    let string = try String(contentsOf: url)
+    self.lines = string.split(separator: "\n")
+  }
+  
+  /// The puzzle input interpreted to be a list of integers.
+  var integers: [Int] {
+    lines.map { Int($0)! }
+  }
+}
+
+struct SlidingWindow<Base> : Collection where Base : Collection {
+  
+  /// The collection being slid over.
+  private(set) var slice: Slice<Base>
+  
+  // MARK: Collection
+  
+  var startIndex: Int { 0 }
+  var endIndex: Int { slice.count }
+  
+  func index(after i: Int) -> Int {
+    i + 1
+  }
+  
+  subscript(position: Int) -> Base.Element {
+    return slice.base[slice.index(slice.startIndex, offsetBy: position)]
+  }
+  
+  // MARK: Adjusting the Window
+  
+  /// Advances the window a given number of times, unless already at the end.
+  mutating func advance(by offset: Int = 1) -> Bool {
+    guard slice.endIndex < slice.base.endIndex else { return false }
+    
+    slice = Slice(base: slice.base,
+                  bounds: slice.index(slice.startIndex, offsetBy: offset)..<slice.index(slice.endIndex, offsetBy: offset))
+    return true
+  }
+}
+
+extension Collection {
+  
+  /// Creates a sliding window starting at the beginning of the collection, containing _length_ elements.
+  func slidingWindow(length: Int) -> SlidingWindow<Self> {
+    let slice = Slice(base: self, bounds: startIndex..<index(startIndex, offsetBy: length))
+    return SlidingWindow(slice: slice)
   }
 }
