754ab4dd3917401d2cc9a46849ce8ec9e8682512
Mike Abdullah <me@mikeabdullah.net>
2021-12-29 11:24:49 +0000

    Get ready for day 1

---
+++ AoC2021/AoC2021Tests/Day 1/Day1.swift
@@ -0,0 +1,24 @@
+//
+//  AoC2021Tests.swift
+//  AoC2021Tests
+//
+//  Created by Mike on 29/12/2021.
+//
+
+import XCTest
+
+class Day1: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  func testPart1() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+}
