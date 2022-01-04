0191fd326be992cb67b83c3526dd7f74410f6b3b
Mike Abdullah <me@mikeabdullah.net>
2022-01-04 18:08:26 +0000

    Helpful init

--- AoC2021/AoC2021Tests/Day6.swift
+++ AoC2021/AoC2021Tests/Day6.swift
@@ -12,31 +12,37 @@ class Day6: XCTestCase {
   func testPart1() throws {
     let input = try PuzzleInput(named: "input-6")
     
-    var simulation = FishSimulation(timerValues: input.lines[0].lazy.split(separator: ",").map { Int($0)! })
-    
+    var simulation = FishSimulation(input: input)
+
     for _ in 1...80 {
       simulation.performSimulation()
     }
     
-    XCTAssertEqual(simulation.timerValues.count, 374994)
+    XCTAssertEqual(simulation.numberOfFish, 374994)
   }
   
   func testPart2() throws {
     let input = try PuzzleInput(named: "input-6")
     
-    var simulation = FishSimulation(timerValues: input.lines[0].lazy.split(separator: ",").map { Int($0)! })
+    var simulation = FishSimulation(input: input)
     
     for _ in 1...256 {
       simulation.performSimulation()
     }
     
-    XCTAssertEqual(simulation.timerValues.count, 374994)
+    XCTAssertEqual(simulation.numberOfFish, 374994)
   }
   
   struct FishSimulation {
     
+    init(input: PuzzleInput) {
+      self.timerValues = input.lines[0].lazy.split(separator: ",").map { Int($0)! }
+    }
+    
     /// The internal timers of each fish.
-    private(set) var timerValues: [Int]
+    private var timerValues: [Int]
+    
+    var numberOfFish: Int { timerValues.count }
     
     /// Simulates a single day's lifecycle
     mutating func performSimulation() {
