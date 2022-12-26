6268377baf10183329d4dff1eb2522bf4d314d43
Mike Abdullah <me@mikeabdullah.net>
2022-12-26 14:23:03 +0000

    Solve part 2

--- AoC22/AoC/Day1.swift
+++ AoC22/AoC/Day1.swift
@@ -20,4 +20,16 @@ final class Day1: XCTestCase {
     let highest = calories.max()!
     XCTAssertEqual(highest, 74711)
   }
+  
+  func testPart2() throws {
+    let input = try PuzzleInput(day: 1).lines
+    let batches = input.split(separator: "")
+    
+    let calories = batches.lazy.map { calorieCounts in
+      calorieCounts.lazy.map { Int($0)! }.reduce(0, +)
+    }.sorted(by: >) // highest calories first
+    
+    let topThreeTotal = calories.prefix(3).reduce(0, +)
+    XCTAssertEqual(topThreeTotal, 209481)
+  }
 }
