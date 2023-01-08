8a57b158d17b356b1ba6ced4cc0c0b211e85a680
Mike Abdullah <me@mikeabdullah.net>
2023-01-08 11:43:01 +0000

    Solve Day 5 part 1

---
+++ AoC22/Day5/Day5.swift
@@ -0,0 +1,58 @@
+//
+//  Day5.swift
+//  AoC
+//
+//  Created by Mike on 08/01/2023.
+//
+
+import XCTest
+import RegexBuilder
+
+final class Day5: XCTestCase {
+  
+  func testPart1() throws {
+    // Load the start state from the input
+    let input = try PuzzleInput(day: 5)
+    let separatorLine = input.lines.firstIndex(of: "")!
+    var stacks = input.stacks(upTo: separatorLine)
+    
+    // Perform the moves
+    for line in input.lines.suffix(from: separatorLine + 1) {
+      let scanner = Scanner(string: String(line))
+      _ = scanner.scanString("move")
+      let numberToMove = scanner.scanInt()!
+      _ = scanner.scanString("from")
+      let fromStack = scanner.scanInt()! - 1
+      _ = scanner.scanString("to")
+      let toStack = scanner.scanInt()! - 1
+      
+      for _ in 0..<numberToMove {
+        let crate = stacks[fromStack].removeLast()
+        stacks[toStack].append(crate)
+      }
+    }
+    
+    // Get top of each stack
+    let top = String(stacks.lazy.map { stack in
+      stack.last!
+    })
+    XCTAssertEqual(top, "TWSGQHNHL")
+  }
+}
+
+private extension PuzzleInput {
+  
+  func stacks(upTo index: Int) -> [[Character]] {
+    let cratesInput = lines.prefix(upTo: index)
+    let stackCount = (cratesInput.last!.count + 1) / 4
+    var stacks = Array(repeating: [Character](), count: stackCount)
+    
+    for line in cratesInput.dropLast() {
+      for (index, crate) in line.dropFirst().striding(by: 4).enumerated() where crate != " " {
+        stacks[index].append(crate)
+      }
+    }
+    
+    return stacks.map { $0.reversed() }
+  }
+}
